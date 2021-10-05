82056d51f1cc7565f2d4e537cd95913a1bb5ecac
Paulo Henrique Jr <paulojack2011@gmail.com>
2021-10-04 22:07:06 -0300

    updates angular

---
+++ angular/currency_converter_project/conversor/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { AppComponent } from "./app.component";
+import { ConversorModule } from "./conversor";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ConversorModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ angular/currency_converter_project/conversor/src/app/conversor/models/conversao-response.model.ts
@@ -0,0 +1,11 @@
+export class ConversaoResponse {
+  constructor(
+    public base: string,
+    public date: string,
+    public rates: any
+
+){}
+
+}
+
+//{"base":"USD","date":"2021-10-04","rates":{"BRL":5.43}}
